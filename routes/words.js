4d5800da1061972f0f4a38351a97d02eac966a0c
Jesus Rodriguez <foxandxss@gmail.com>
2015-05-04 20:08:16 +0200

    Fix overflow on random and change delete for omit

--- routes/words.js
+++ routes/words.js
@@ -1,12 +1,13 @@
 var router     = require('koa-router')();
 var jwtChecker = require('../utils/jwtChecker');
 var words      = require('../words/words');
+var _          = require('lodash');
 
 router.get('/api/random-word', function* () {
   var user = yield jwtChecker(this.header);
   var word = words.getRandomWord();
   if ( ! user) {
-    delete word.english;
+    word = _.omit(word, 'english');
   }
   this.body = word;
 });
--- words/words.js
+++ words/words.js
@@ -1,7 +1,7 @@
 var words = require('./words.json').words;
 
 exports.getRandomWord = function() {
-  var totalWords = words.length;
+  var totalWords = words.length - 1;
   var rand = Math.ceil(Math.random() * totalWords);
   return words[rand];
 };
